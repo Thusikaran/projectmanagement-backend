77d2a073b728f4b9ee891e8688c542ec576c0de0
Thusikaran <pushparasa.thusikaran@gmail.com>
2024-10-04 10:12:00 +0530

    spring srcurity initialise

---
+++ projectmanagementsystem/src/main/java/com/Zosh/projectmanagementsystem/config/AppConfig/js.java
@@ -0,0 +1,10 @@
+package com.Zosh.projectmanagementsystem.config.AppConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class js {
+
+}
